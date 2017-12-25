8de1c1762d5050c6388659ed349100d0abdea2ed
shinyounglee <shinyounglee@172.30.1.50>
2017-12-25 23:40:26 +0900

    구구단 5단 'scannr'클래스를 사용하여 사용자 입력값을 받는 방

---
+++ src/leeshinyoung/com/github/eclipse/Gugudan_variableinput.java
@@ -0,0 +1,24 @@
+package leeshinyoung.com.github.eclipse;
+
+import java.util.Scanner;
+
+public class Gugudan_variableinput {
+	
+	public static void main(String[] args) {
+		int result = (4 * 1);
+		//5
+		System.out.println(result);
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println(number * 1);
+		System.out.println(number * 2);
+		System.out.println(number * 3);
+		System.out.println(number * 4);
+		System.out.println(number * 5);
+		System.out.println(number * 6);
+		System.out.println(number * 7);
+		System.out.println(number * 8);
+		System.out.println(number * 9);
+	}
+}
