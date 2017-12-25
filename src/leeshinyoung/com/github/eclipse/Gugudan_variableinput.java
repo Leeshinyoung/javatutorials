1438216ad4396b4b35c4a07dfc6feeb3bd0e9010
shinyounglee <shinyounglee@172.30.1.50>
2017-12-25 23:49:48 +0900

    혼자하

--- src/leeshinyoung/com/github/eclipse/Gugudan_variableinput.java
+++ src/leeshinyoung/com/github/eclipse/Gugudan_variableinput.java
@@ -6,11 +6,12 @@ public class Gugudan_variableinput {
 	
 	public static void main(String[] args) {
 		int result = (4 * 1);
-		//5
 		System.out.println(result);
-		System.out.println("구구단 중 출력할 단은? : ");
+		//5
+		System.out.println("몇단을 출력하실 건가요? : ");
 		Scanner scanner = new Scanner(System.in);
 		int number = scanner.nextInt();
+		
 		System.out.println(number * 1);
 		System.out.println(number * 2);
 		System.out.println(number * 3);
