1d419135fbf9ca782c03809fcba59ff899bd2e03
shinyounglee <shinyounglee@172.30.1.50>
2017-12-26 00:01:23 +0900

    6단- while문 7단 -for

---
+++ src/leeshinyoung/com/github/eclipse/Gugudan_for_while.java
@@ -0,0 +1,18 @@
+package leeshinyoung.com.github.eclipse;
+
+public class Gugudan_for_while {
+
+	public static void main(String[] args) {
+		//while을 이용한 6단 
+		int i = 1;
+		while (i < 10) {
+			System.out.println(6 * i);
+			i += 1;
+		}
+		//for를 이용한 7단 
+		for (int j = 1; j<10; j++) {
+			System.out.println(7 * j);	
+		}
+	}
+}
+
